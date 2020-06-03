0f32de11b05f9e692edbc2491730950f5936968b
Edson Gomes <edyz86@hotmail.com>
2020-06-03 18:52:11 -0300

    Finalizao - Arquitetura e testes no Node.js

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+
+describe('CreateUser', () => {
+  it('shoulbe able to create a new user', async () => {
+    const fakeHashProvider = new FakeHashProvider();
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+    const user = await createUser.execute({
+      name: 'Edson',
+      email: 'edson@edson.com',
+      password: '123456',
+    });
+    expect(user).toHaveProperty('id');
+  });
+
+  it('shoulbe not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+    await createUser.execute({
+      name: 'Edson',
+      email: 'edson@edson.com',
+      password: '123456',
+    });
+    expect(
+      createUser.execute({
+        name: 'Edson',
+        email: 'edson@edson.com',
+        password: '123456',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,75 @@
+import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import UpdateAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+
+describe('UpdateUserAvatar', () => {
+  it('shoulbe able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateAvatar = new UpdateAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider
+    );
+    const user = await fakeUsersRepository.create({
+      name: 'Edson',
+      email: 'edson@edson.com',
+      password: '123456',
+    });
+
+    await updateAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg',
+    });
+    expect(user.avatar).toBe('avatar.jpg');
+  });
+
+  it('should not be able to update avatar from non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateAvatar = new UpdateAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider
+    );
+
+    expect(
+      updateAvatar.execute({
+        user_id: 'non-existing-user',
+        avatarFilename: 'avatar.jpg',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to delete an old avatar when updating a new one', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+    // podemos observar se a função foi executada ou não, com esse spy
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const updateAvatar = new UpdateAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider
+    );
+    const user = await fakeUsersRepository.create({
+      name: 'Edson',
+      email: 'edson@edson.com',
+      password: '123456',
+    });
+
+    await updateAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg',
+    });
+
+    await updateAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar2.jpg',
+    });
+    expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+    expect(user.avatar).toBe('avatar2.jpg');
+  });
+});
